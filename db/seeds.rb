8214d3557fcd60fac1832d2841da70038170fc0e
sophie alexandre <sophie.alec@gmail.com>
2020-09-18 16:44:37 -0400

    heroku

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+
+
+Creation.create(nom: "Robe", description: "Stylée", difficulte: 1, created_at: "2020-09-15 17:21:07", updated_at: "2020-09-15 17:21:07", photo1: nil, photo2: nil, photo3: nil, photo4: nil)
+
+Creation.create(nom: "Jupe", description: "Classe", difficulte: 3, created_at: "2020-09-15 17:21:07", updated_at: "2020-09-15 17:21:07", photo1: nil, photo2: nil, photo3: nil, photo4: nil)
+
+Creation.create(nom: "Pantalon", description: "Décontracté", difficulte: 2, created_at: "2020-09-15 17:21:07", updated_at: "2020-09-15 17:21:07", photo1: nil, photo2: nil, photo3: nil, photo4: nil)
+
+Creation.create(nom: "Combi", description: "Stylée", difficulte: 1, created_at: "2020-09-15 17:21:07", updated_at: "2020-09-15 17:21:07", photo1: nil, photo2: nil, photo3: nil, photo4: nil)
+
